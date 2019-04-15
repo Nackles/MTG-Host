3b425d7814a5597574f60939474ca4201af8e6ae
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-04-15 13:19:18 -0700

    Got auth test working

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(passport.session());
 
 // Requiring our routes
 require("./routes/htmlRoutes.js")(app);
-require("./routes/apiRoutes.js")(app);
+require("./routes/auth-api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
