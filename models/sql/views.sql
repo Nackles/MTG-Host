30c86174efe0fc9b92c98458713663b0c509897a
Bronson <bt98199@gmail.com>
2019-04-16 11:25:59 -0700

    Updating the SQL source files
    Pluralized the table names

--- models/sql/views.sql
+++ models/sql/views.sql
@@ -1,16 +1,16 @@
-drop view winloss;
-create view winloss as 
-select player.id, player.name as player, player.img_link as icon,
-(select count(*) from result where player.id = result.player_id and result.outcome = 'W') as w,
-(select count(*) from result where player.id = result.player_id and result.outcome = 'L') as l,
-round(((select count(*) from result where player.id = result.player_id and result.outcome = 'W') / 
-((select count(*) from result where player.id = result.player_id and result.outcome = 'L') + 
- (select count(*) from result where player.id = result.player_id and result.outcome = 'L'))),3) as pct, 
- (select count(*) from result where player.id = result.player_id and result.outcome = 'L') + 
- (select count(*) from result where player.id = result.player_id and result.outcome = 'L') as num_games,
-(select max(updated_at) from result where player.id = result.player_id and result.outcome in ('W','L')) as last_played,
-(select round(avg(duration)/60) from result where player.id = result.player_id and result.outcome in ('W','L')) as avg_duration
-from player
-order by player.id
+drop view winlosses;
+create view winlosses as 
+select players.id, players.name as player, players.img_link as icon,
+(select count(*) from results where players.id = results.player_id and results.outcome = 'W') as w,
+(select count(*) from results where players.id = results.player_id and results.outcome = 'L') as l,
+round(((select count(*) from results where players.id = results.player_id and results.outcome = 'W') / 
+((select count(*) from results where players.id = results.player_id and results.outcome = 'L') + 
+ (select count(*) from results where players.id = results.player_id and results.outcome = 'L'))),3) as pct, 
+ (select count(*) from results where players.id = results.player_id and results.outcome = 'L') + 
+ (select count(*) from results where players.id = results.player_id and results.outcome = 'L') as num_games,
+(select max(updated_at) from results where players.id = results.player_id and results.outcome in ('W','L')) as last_played,
+(select round(avg(duration)/60) from results where players.id = results.player_id and results.outcome in ('W','L')) as avg_duration
+from players
+order by players.id
 ;
-select * from winloss;
+select * from winlosses;
